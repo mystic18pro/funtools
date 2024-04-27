ee99b6790730dba45e79120fba9731b7823e657b
Basic-Funda-Developer <83956232+mystic18pro@users.noreply.github.com>
2024-04-27 18:51:10 +0530

    v0.1.0 - Revamped the performance of 'Increase Vocabulary' tool. Added Functionality to main.py, now just run main.py to access all tools, no need to run tools individually.

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from tools.increaseVocabulary import randomDefinition
+from tools.increaseVocabulary import printDefinition, getDefinition, getRandomWord
+import sys 
+tools = [
+'Increase Vocabulary'
+]
 
-l = {'Random Definition':  randomDefinition()}
+def useTool():
+    print("Enter a tool number: ")
+    num = 1
 
-
+    for tool in tools:
+        print(f'{num}. {tool}')
+    userChoice = input(": ")
+    try:
+        userChoice = int(userChoice)
+    except ValueError:
+        print("Invalid Choice")
+        useTool()
+    if userChoice == 1:
+        word = getRandomWord()
+        validOrNot, definition = getDefinition(word)
+        printDefinition(word,validOrNot,definition)
+    restartOrNot = input("Do you want to use another tool? (y/n): ").lower()
+    if restartOrNot == 'y':
+        useTool()
+    else:
+        sys.exit()
+useTool()
--- tools/increaseVocabulary.py
+++ tools/increaseVocabulary.py
@@ -2,23 +2,27 @@ import requests
 from tools.APIKEY import apiKey
 
 randWordsURL = 'https://api.api-ninjas.com/v1/randomword'
-randWordsResponse = requests.get(randWordsURL, headers={'X-Api-Key': apiKey})
+def getRandomWord():
+    randWordsResponse = requests.get(randWordsURL, headers={'X-Api-Key': apiKey})
 
-word = randWordsResponse.json()['word']
+    return randWordsResponse.json()['word']
 
-definitionURL = 'https://api.api-ninjas.com/v1/dictionary?word={}'.format(word)
-definitionResponse = requests.get(definitionURL, headers={'X-Api-Key': apiKey})
 
-validOrNot = definitionResponse.json()['valid']
-definition = definitionResponse.json()['definition']
+def getDefinition(word):
+    definitionURL = 'https://api.api-ninjas.com/v1/dictionary?word={}'.format(word)
+    definitionResponse = requests.get(definitionURL, headers={'X-Api-Key': apiKey})
+
+    validOrNot = definitionResponse.json()['valid']
+    definition = definitionResponse.json()['definition']
+    return validOrNot,definition
+
 
 
 # Printing part
-def randomDefinition():
+def printDefinition(word,validOrNot,definition):
     if validOrNot == True:
         print(f"Increase your vocabulary!!\nWord: {word}\nDefinition: {definition}")
     else: 
         print(f"Sorry!! There is some issue. Please try again.")
-        randomDefinition(word, validOrNot, definition)
 
 
